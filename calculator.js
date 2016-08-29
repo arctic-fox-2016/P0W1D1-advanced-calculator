b69992320becbe545d5209074d3973cae2d6c3aa
Septhianto Diga & Tevin <pair+digachandra.tevinstein@hacktiv8.com>
2016-08-29 17:51:26 +0700

    advanced calculator kelar

--- calculator.js
+++ calculator.js
@@ -2,7 +2,39 @@
 
 export class Calculator {
   //write your code here
+  constructor(x){
+    this.x = x;
+  }
+
+  add(y){
+    return this.x + y;
+  }
+
+  substract(y){
+    return this.x - y;
+  }
+
+  multiply(y){
+    return this.x * y;
+  }
+
+  divide(y){
+    return this.x / y;
+  }
+
+  power(y){
+    return Math.pow(this.x, y);
+  }
+
+  root(){
+    return Math.sqrt(this.x);
+  }
+
+  circle(y){
+    return Math.pow(this.x, 2) * Math.PI;
+  }
 }
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let advnc_calc = new Calculator(10);
+
+console.log('ANGKA AWAL 10!');
+console.log('add 10: '+ advnc_calc.add(10) + '\n');
+console.log('substract 10: '+ advnc_calc.substract(10) + '\n');
+console.log('multiply 10: '+ advnc_calc.multiply(10) + '\n');
+console.log('divide 10: '+ advnc_calc.divide(10) + '\n');
+console.log('power 10: '+ advnc_calc.power(10) + '\n');
+console.log('root 10: '+ advnc_calc.root() + '\n');
+console.log('circle 10: '+ advnc_calc.circle(10) + '\n');
