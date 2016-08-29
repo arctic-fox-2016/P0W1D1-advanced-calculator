6622da0c67fd82a4eff71d235cdb9fa3c9c51606
Ahmad Ahmadi & Ahyana Rizky Pratama <pair+ahmadahmadi14.ahyanarizky@hacktiv8.com>
2016-08-29 18:19:59 +0700

    calculator_ahmadahyana

--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let hitung = new Calculator();
+console.log(hitung.add(2,3));
+console.log(hitung.substract(5,3));
+console.log(hitung.multiply(2,3));
+console.log(hitung.divide(8,4));
+console.log(hitung.pangkat(2,3));
+console.log(hitung.root(16));
+console.log(hitung.circle(7));
