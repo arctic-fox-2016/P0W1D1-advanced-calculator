f56a2f6266170596264038c116255033071cb092
name": null, & Andrew Tandiawan <pair+ajilantang.atandiawan@hacktiv8.com>
2016-08-29 19:04:20 +0700

    tugas kalkulator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,38 @@
 "use strict";
-
+const PI = 3.14;
 export class Calculator {
+  
+  addition(a,b){
+    return a+b;
+  }
+  substract(a,b){
+    return a-b;
+  }
+  multiply(a,b){
+    return a*b;
+  }
+  divide(a,b){
+    return a/b;
+  }
+  power(a,b){
+    return Math.pow(a,b);
+  }
+  sqrt(a){
+    return Math.pow(a,0.5);
+  }
+  area(a){
+    return PI*a*a;
+  }
   //write your code here
 }
 
+//addition
+//substract
+//multiply
+//dive
+//power
+//sqrt
+//circle
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let calculator = new Calculator();
+console.log(calculator.addition(3,4));
+console.log(calculator.substract(3,4));
+console.log(calculator.multiply(3,4));
+console.log(calculator.divide(3,4));
+console.log(calculator.power(3,4));
+console.log(calculator.sqrt(4));
+console.log(calculator.area(7));
