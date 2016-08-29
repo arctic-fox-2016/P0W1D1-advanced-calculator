5ae9a7fa85fb40bbabea4d9671e4a71a3e391bf0
name": null, & Kun Anggiar Lanang <pair+ivangerard.kun7@hacktiv8.com>
2016-08-29 17:54:21 +0700

    Advance Calc

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 "use strict";
 
 export class Calculator {
-  //write your code here
+  //writcle your code here
 }
 
 /** note : you can use several features from ecmascript, such as:
@@ -11,3 +11,44 @@ export class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
+const PI = 3.14;
+
+class Calculator {
+  constructor(x){
+    this.x =x;
+  }
+  add (a){
+    this.x +=a;
+    return this;
+  }
+  substract(b){
+    this.x -=b;
+    return this;
+  }
+  multiply (c){
+    this.x *=c;
+    return this;
+  }
+  divide (d){
+    this.x /=d;
+    return this;
+  }
+  pangkat (e){
+    this.x = Math.pow(this.x,e);
+    return this;
+  }
+  akar (f){
+    this.x = Math.sqrt(this.x);
+    return this;
+  }
+  lingkaran(r){
+    return PI*r*r;
+  }
+  result(){
+    console.log(this.x);
+  }
+}
+
+let test = new Calculator(14);
+console.log(test.lingkaran(7));
