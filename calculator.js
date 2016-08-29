6055887e562402b55a718c64d88ac299301db69b
Ryan Andhaka & Sahbana <pair+ryanandhaka.sahbanagold@hacktiv8.com>
2016-08-29 20:34:19 +0700

    advancedcalculatorberes

--- calculator.js
+++ calculator.js
@@ -1,9 +1,53 @@
 "use strict";
 
+const PI = 3.1415
+
 export class Calculator {
   //write your code here
-}
+  constructor(x){
+    this.x = x
+  }
 
+  add (y){
+    this.x += y
+    return this
+  }
+
+  sub (z){
+    this.x -= z
+    return this
+  }
+
+  multiply (k){
+    this.x *= k
+    return this
+  }
+
+  div (b){
+    this.x /= b
+    return this
+  }
+
+  power (p){
+    this.x ^= p
+    return this
+  }
+
+  sqrt (a){
+    this.x = Math.sqrt(a)
+    return this
+  }
+
+  area (r){
+    this.x = PI*r*r
+    return this
+  }
+
+  result(){
+    console.log(this.x)
+  }
+
+}
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
--- main.js
+++ main.js
@@ -3,3 +3,5 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+var hitung = new Calculator(7)
+hitung.add(5).sub(2).multiply(10).div(4).result()
